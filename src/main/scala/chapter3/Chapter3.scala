8802765aef1f9ed1cd3fe6472cc6bd5bdc8182ff
Mikel <mikel.alvarez@wyred.biz>
2017-11-07 12:50:33 +0100

    Added more exercises of chapter 3

--- src/main/scala/chapter3/Chapter3.scala
+++ src/main/scala/chapter3/Chapter3.scala
@@ -7,7 +7,7 @@ object Chapter3 {
 
   case object Nil extends List[Nothing]
 
-  case class Cons[+A](head: A, tail: List[A])
+  case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
   // 3.2
   def tail[A](list:List[A]):List[A] = list match {
@@ -21,6 +21,48 @@ object Chapter3 {
     case list:List[A] => Cons(head,list)
     case Nil => Cons(head,Nil)
   }
+
+  // 3.4
+
+  def drop[A](n: Int, list: List[A]):List[A] = n match {
+    case 0 => list
+    case 1 => list match  {
+      case Cons(head:A,tail:List[A]) => tail
+      case Nil => Nil
+    }
+    case _ =>list match  {
+      case Cons(head:A,tail:List[A]) => drop(n-1,tail)
+      case Nil => Nil
+    }
+  }
+
+  // 3.4
+
+  def dropWhile[A](list: List[A], f: A => Boolean):List[A] = list match {
+    case Cons(head:A,tail:List[A]) => if (f(head)) dropWhile(list,f) else list
+    case Nil => Nil
+    }
+
+
+  // 3.5
+
+  def init[A](l: List[A]):List[A] = {
+    l match {
+      case Cons(head:A, tail:List[A]) => Cons(head,init(tail))
+      case Cons(head:A, Nil) => Nil
+      case Nil => sys.error("Error, init of nil")
+    }
+  }
+
+  //3.7
+
+  def product(list:List[Double]):Double = list match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x, xs) => x * product(xs)
+  }
+
+
 
 
 
